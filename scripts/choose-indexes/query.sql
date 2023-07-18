8530afb9dc44f9969c4b4cd76e7a36096c12f794
seesharprun <sidandrews@microsoft.com>
2023-07-18 14:16:09 -0400

    Add `CHOOSE`

---
+++ scripts/choose-indexes/query.sql
@@ -0,0 +1,8 @@
+SELECT VALUE {
+    index_0: CHOOSE(0, "abc", 1, true, [1]),
+    index_1: CHOOSE(1, "abc", 1, true, [1]),
+    index_2: CHOOSE(2, "abc", 1, true, [1]),
+    index_3: CHOOSE(3, "abc", 1, true, [1]),
+    index_4: CHOOSE(4, "abc", 1, true, [1]),
+    index_5: CHOOSE(5, "abc", 1, true, [1])
+}
