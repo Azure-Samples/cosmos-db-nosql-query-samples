2ef838da5d4d04964b461494d8d8f14e21aea143
seesharprun <sidandrews@microsoft.com>
2023-07-19 14:37:10 -0400

    Add `DEGREES`

---
+++ scripts/degrees/query.sql
@@ -0,0 +1,6 @@
+SELECT VALUE {
+    radiansHalfPiToDegrees: DEGREES(1.5707963267948966),
+    radiansPiToDegrees: DEGREES(3.141592653589793),
+    radiansOneAndHalfPiToDegrees: DEGREES(4.71238898038469),
+    radiansDoublePiToDegrees: DEGREES(6.283185307179586)
+}
