58619f928adc36878d936ee94d7a8eea38658f70
seesharprun <sidandrews@microsoft.com>
2023-07-19 12:26:39 -0400

    Add `DateTimeFromParts`

---
+++ scripts/datetimefromparts/query.sql
@@ -0,0 +1,7 @@
+SELECT VALUE {
+    constructMinArguments: DateTimeFromParts(2017, 4, 20),
+    constructMinEquivalent: DateTimeFromParts(2017, 4, 20, 0, 0, 0, 0),
+    constructAllArguments: DateTimeFromParts(2017, 4, 20, 13, 15, 20, 3456789),
+    constructPartialArguments: DateTimeFromParts(2017, 4, 20, 13, 15),
+    constructInvalidArguments: DateTimeFromParts(-2000, -1, -1)
+}
