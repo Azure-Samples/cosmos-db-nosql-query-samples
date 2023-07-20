5d2bd19fcd240152192a26d0a5f692d8be50faea
seesharprun <sidandrews@microsoft.com>
2023-07-19 21:16:37 -0400

    Add `GetCurrentTicksStatic`

---
+++ scripts/getcurrentticksstatic/result.novalidate.sql
@@ -0,0 +1,20 @@
+[
+  {
+    "id": "1",
+    "partitionKey": "A",
+    "nonStaticTicks": 16879779663422236,
+    "staticTicks": 16879779663415572
+  },
+  {
+    "id": "2",
+    "partitionKey": "A",
+    "nonStaticTicks": 16879779663422320,
+    "staticTicks": 16879779663415572
+  },
+  {
+    "id": "3",
+    "partitionKey": "B",
+    "nonStaticTicks": 16879779663422380,
+    "staticTicks": 16879779663421680
+  }
+]
