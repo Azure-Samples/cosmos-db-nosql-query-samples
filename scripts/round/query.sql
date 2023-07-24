02ce3aaa23bf42d3d19411a1cfccf40b5cc70db6
seesharprun <sidandrews@microsoft.com>
2023-07-24 09:59:04 -0400

    Add `ROUND`

---
+++ scripts/round/query.sql
@@ -0,0 +1,7 @@
+SELECT VALUE {
+    roundTwoPointFour: ROUND(2.4),
+    roundTwoPointSix: ROUND(2.6),
+    roundTwoPointFive: ROUND(2.5),
+    roundNegativeTwoPointFour: ROUND(-2.4),
+    roundNegativeTwoPointSix: ROUND(-2.6)
+}
