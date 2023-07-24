a37be0b1d1190f8f826b4109c8f912c8e14b3a1c
seesharprun <sidandrews@microsoft.com>
2023-07-24 13:27:52 -0400

    Add `UPPER`

---
+++ scripts/upper/query.sql
@@ -0,0 +1,7 @@
+SELECT VALUE {
+    lowercase: UPPER("adventureworks"),
+    uppercase: UPPER("ADVENTUREWORKS"),
+    camelCase: UPPER("adventureWorks"),
+    pascalCase: UPPER("AdventureWorks"),
+    upperSnakeCase: UPPER("ADVENTURE_WORKS")
+}
