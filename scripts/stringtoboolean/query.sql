df7a365f9dabab2a981ec9bf1fdf2c8673853d6c
seesharprun <sidandrews@microsoft.com>
2023-07-24 14:05:04 -0400

    Update `StringToBoolean`

--- scripts/stringtoboolean/query.sql
+++ scripts/stringtoboolean/query.sql
@@ -4,13 +4,6 @@ SELECT VALUE {
     parseWithSuffix: StringToBoolean("  false"),
     parseWithWhitespace: StringToBoolean("  false  "),
     parseBoolean: StringToBoolean(true),
-    parseInteger: StringToBoolean("100"), 
-    parseDecimal: StringToBoolean("3.14"),
-    parseScientific: StringToBoolean("-1.79769e+308"),
-    parseInvalid: StringToBoolean("Hello"),
     parseUndefined: StringToBoolean(undefined),
-    parseNull: StringToBoolean(null),
-    parseNaN: StringToBoolean(NaN),
-    parseArray: StringToBoolean([ 1, 2, 3 ]),
-    parseInfinity: StringToBoolean(Infinity)
+    parseNull: StringToBoolean(null)
 }
