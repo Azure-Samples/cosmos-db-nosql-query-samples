b7e692e0743140d8f1d22cd3e4b3469e01b42be3
seesharprun <sidandrews@microsoft.com>
2023-07-24 13:35:24 -0400

    Update `StringToNumber`

--- scripts/stringtonumber/query.sql
+++ scripts/stringtonumber/query.sql
@@ -3,5 +3,11 @@ SELECT VALUE {
     parseDecimal: StringToNumber("3.14"),
     parseWithWhitespace: StringToNumber("   60   "), 
     parseScientific: StringToNumber("-1.79769e+308"),
-    parseInvalid: StringToNumber("Hello")
+    parseInvalid: StringToNumber("Hello"),
+    parseUndefined: StringToNumber(undefined),
+    parseNull: StringToNumber(null),
+    parseNaN: StringToNumber(NaN),
+    parseArray: StringToNumber([ 1, 2, 3 ]),
+    parseInfinity: StringToNumber(Infinity),
+    parseBoolean: StringToNumber(true)
 }
