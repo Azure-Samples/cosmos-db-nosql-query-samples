beeb53a88eaee8272f292fd0fa633e4a404e7b0b
seesharprun <sidandrews@microsoft.com>
2023-07-31 11:13:31 -0400

    Add `OFFSET LIMIT`

---
+++ scripts/offset-limit/seed.sql
@@ -0,0 +1,27 @@
+[
+  {
+    "id": "5929071063",
+    "name": "Sawyer Miller",
+    "team": "Leadership team"
+  },
+  {
+    "id": "1599541170",
+    "name": "Jennifer Wilkins",
+    "team": "Leadership team"
+  },
+  {
+    "id": "9461206378",
+    "name": "Hannah Haynes",
+    "team": "Leadership team"
+  },
+  {
+    "id": "3261752803",
+    "name": "Isaac Talbot",
+    "team": "Leadership team"
+  },
+  {
+    "id": "5011271389",
+    "name": "Riley Johnson",
+    "team": "Leadership team"
+  }
+]
