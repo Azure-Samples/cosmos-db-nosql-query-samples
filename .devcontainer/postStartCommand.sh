f4c92bdc83d728e115e96a32c10e7a505a46852d
Sidney Andrews <sidandrews@microsoft.com>
2024-07-09 18:25:17 -0400

    Update workflow

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,6 +1,6 @@
 echo "Installing emulator certificate"
 
-curl --insecure https://cosmos.domain:8081/_explorer/emulator.pem > ~/emulatorcert.crt
+curl --insecure https://cosmosdb.domain:8081/_explorer/emulator.pem > ~/emulatorcert.crt
 
 cp ~/emulatorcert.crt /usr/local/share/ca-certificates/
 
